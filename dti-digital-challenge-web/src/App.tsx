95ebc3aeb503c0c4e32b61c99030720ec72ad829
Pablo <pablolucio_@hotmail.com>
2024-12-01 16:31:58 -0300

    fix: fixes react-modal element reference

--- dti-digital-challenge-web/src/App.tsx
+++ dti-digital-challenge-web/src/App.tsx
@@ -1,9 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Modal from "react-modal";
+import { Toaster } from "./components/ui/toaster";
 import AppRouter from "./routes";
 import "./styles/globals.css";
-import { Toaster } from "./components/ui/toaster";
 
 const queryClient = new QueryClient();
+
+Modal.setAppElement("#root");
 
 function App() {
   return (
